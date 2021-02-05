6bc5cca5b1940bf7f1f5703d4bdb8d9f5e440b9c
Hannah Akhtar <hannahaakhtar@gmail.com>
2021-02-05 15:08:25 +0000

    logo update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Logo2 from '../images/Logo2.png'
 
 export default function Navbar() {
   return <div className="navbar is-primary">
     <div className="navbar-start">
-      <img className="logo" src={Logo2} alt="logo" width="100" height="20"/>
+      <img className="logo" src="https://i.imgur.com/9j8FdC2.png" alt="logo" width="100" height="20"/>
     </div>
     <div className="navbar-end">
       <div className="navbar-brand">
